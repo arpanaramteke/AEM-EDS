ecb36a7e2b1cd28e8a984115c6888b59f568576e
Arpana <arpana.ramteke@netcentric.biz>
2026-04-03 11:15:04 +0530

    added w3schools js

--- blocks/custom/custom.js
+++ blocks/custom/custom.js
@@ -1,37 +1,50 @@
-/*
- * Accordion Block
- * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
- */
 
-export default function decorate(block) {
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+/*export default function decorate(block) {
   const tabContainer = document.createElement('div');
   tabContainer.className = 'tab';
 
   const contentContainer = document.createElement('div');
 
   [...block.children].forEach((row, index) => {
-    const labelDiv = row.children[0]; // first div = label
-    const contentDiv = row.children[1]; // second div = body
+    const labelDiv = row.children[0]; 
+    const contentDiv = row.children[1]; 
 
-    // create tab button
     const button = document.createElement('button');
     button.className = 'tablinks';
     button.textContent = labelDiv.textContent;
 
-    // create tab content
     const tabContent = document.createElement('div');
     tabContent.className = 'tabcontent';
     tabContent.append(...contentDiv.childNodes);
-    tabContent.style.display = 'none'; // hide initially
+    tabContent.style.display = 'none'; 
 
-    // hover effect (show content)
     button.addEventListener('mouseover', (e) => {
-      // hide all tab contents
+
       contentContainer.querySelectorAll('.tabcontent').forEach((c) => c.style.display = 'none');
-      // remove active class from all buttons
+
       tabContainer.querySelectorAll('.tablinks').forEach((b) => b.classList.remove('active'));
-      // show current
+
       tabContent.style.display = 'block';
       e.currentTarget.classList.add('active');
     });
@@ -55,4 +68,4 @@ export default function decorate(block) {
   const clearfix = document.createElement('div');
   clearfix.className = 'clearfix';
   block.appendChild(clearfix);
-}
+}*/
