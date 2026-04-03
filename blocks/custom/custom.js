b3909cec7aad257550ecb245599498e97853ec9d
Arpana <arpana.ramteke@netcentric.biz>
2026-04-03 11:17:05 +0530

    custom js

--- blocks/custom/custom.js
+++ blocks/custom/custom.js
@@ -1,26 +1,10 @@
+/*
+ * Accordion Block
+ * Recreate an accordion
+ * https://www.hlx.live/developer/block-collection/accordion
+ */
 
-function openCity(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-/*export default function decorate(block) {
+export default function decorate(block) {
   const tabContainer = document.createElement('div');
   tabContainer.className = 'tab';
 
@@ -68,4 +52,4 @@ function openCity(evt, cityName) {
   const clearfix = document.createElement('div');
   clearfix.className = 'clearfix';
   block.appendChild(clearfix);
-}*/
+}
